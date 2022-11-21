2195fb6ed7f511b6f522b27d358171c6aacabb92
Hi, I'm Souleymane Walda <115745530+DIDANE96@users.noreply.github.com>
2022-11-21 17:08:19 +0800

    Add files via upload

---
+++ KaiboApplication.java
@@ -0,0 +1,13 @@
+package com.kaibo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KaiboApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KaiboApplication.class, args);
+	}
+
+}
